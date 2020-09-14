2d487f4c6a4dc1e0327753eb873fe54888b0ed66
Linda "zig" Speight <linda@reachmaine.com>
2020-09-14 17:09:56 -0400

    working on redirects

--- mcmullen/functions.php
+++ mcmullen/functions.php
@@ -9,3 +9,22 @@ add_action( 'wp_enqueue_scripts', 'divi__child_theme_enqueue_styles' );
 //require_once(get_stylesheet_directory().'/custom/reach-CTAs.php');
 require_once(get_stylesheet_directory().'/custom/language.php');
 require_once(get_stylesheet_directory().'/custom/divi.php');
+
+
+
+
+
+/**
+ * Check whether the page_id query string has been set,
+ * and if so, redirect the user to homepage
+ */
+add_action('template_redirect','wpse339255_redirect_old_urls');
+function wpse339255_redirect_old_urls(){
+    // Check if the page_id query string is set
+    if( isset( $_GET['page_id'] ) && ! empty( $_GET['page_id'] ) ){
+        // Do the redirect
+        wp_redirect( home_url() );
+        die;
+    }
+
+}
